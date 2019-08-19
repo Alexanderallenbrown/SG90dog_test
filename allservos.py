66346b993012f270b5f165dc71ab2c839fcd5052
Alexander Brown <brownaa@lafayette.edu>
2019-08-19 10:23:36 -0400

    added all legs test

---
+++ allservos.py
@@ -0,0 +1,49 @@
+# Servo2.py
+# Two servo motors driven by PCA9685 chip
+
+from smbus import SMBus
+from PCA9685 import PWM
+import time
+
+fPWM = 50
+i2c_address = 0x40 # (standard) adapt to your module
+channel = 0 # adapt to your wiring
+a = 8.5 # adapt to your servo
+b = 2  # adapt to your servo
+
+def setup():
+    global pwm
+    bus = SMBus(1) # Raspberry Pi revision 2
+    pwm = PWM(bus, i2c_address)
+    pwm.setFreq(fPWM)
+
+
+def setHips(angle):
+    duty = a / 180 * angle + b
+    for ch in range(8,12):
+        pwm.setDuty(ch,duty)
+
+def setLegs(angle):
+    duty = a / 180 * angle + b
+    for ch in range(0,8):
+        pwm.setDuty(ch,duty)
+
+def setDirection(direction):
+    duty = a / 180 * direction + b
+    pwm.setDuty(channel, duty)
+    print "direction =", direction, "-> duty =", duty
+    time.sleep(1) # allow to settle
+   
+print "starting"
+setup()
+
+tstart = time.time()
+
+while True:
+    t = time.time()-tstart
+    alegs = 90+15*sin(t*3.14)
+    setLegs(alegs)
+    setHips(90)
+    time.sleep(0.01)
+
+

ddea966beb277e775fd3fe45b44aa7400eb29ead
Alexander Brown <brownaa@lafayette.edu>
2019-09-15 22:26:27 -0400

    testing walk

--- robotwalk.py
+++ robotwalk.py
@@ -51,7 +51,7 @@ flLeg = Leg(side=2)
 lrLeg = Leg(side=2)
 rrLeg = Leg(side=1)
 
-walker = Walk(stride_height=-0.01,stride_length =- .04)
+walker = Walk(stride_height=-0.01,stride_length =.04)
 
 p = 0
 

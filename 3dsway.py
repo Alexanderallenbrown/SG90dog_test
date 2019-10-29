effaf5b60f7c4b7a2998a80dc38d42b98c4409b3
Alexander Brown <brownaa@lafayette.edu>
2019-10-29 11:30:51 -0400

    3d updates

--- 3dsway.py
+++ 3dsway.py
@@ -102,13 +102,13 @@ def doStompL(freq,amp,t):
     yfl = amp*sin(freq*t)
     zfl = amp*sin(2*freq*t)
     xfr = 0#amp*sin(freq*t)
-    yfr = -amp
+    yfr = amp
     zfr = 0
     xlr = 0#amp*sin(freq*t)
-    ylr = amp
+    ylr = -amp
     zlr = 0
     xrr = 0#amp*sin(freq*t)
-    yrr = -amp
+    yrr = amp
     zrr = 0
     return xfl,yfl,zfl,xfr,yfr,zfr,xlr,ylr,zlr,xrr,yrr,zrr
 

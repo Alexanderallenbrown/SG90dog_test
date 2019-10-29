4787e40c822316635e4d4393ca50da3f78a38d09
Alexander Brown <brownaa@lafayette.edu>
2019-10-29 11:49:27 -0400

    3d updates

--- 3dsway.py
+++ 3dsway.py
@@ -117,7 +117,7 @@ while True:
     #setHips(90)
     t = time.time()-starttime
     # print t
-    beats = int(2*pi/freq*t)
+    beats = int(t/(bpm/60.0))
     measures = int(beats/4)
     print beats, measures
 
